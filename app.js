ef11c1dfc5ee396997b6f19fb180d650c7dbee3f
Michael <88445485+mjnyland@users.noreply.github.com>
2024-02-23 21:36:28 -0500

    removed some console logs

--- app.js
+++ app.js
@@ -2,19 +2,10 @@ let faqContainer = document.querySelectorAll(".faq-container");
 let active = false;
 
 faqContainer.forEach((faq) => {
-  console.log(faq);
   faq.addEventListener("click", (e) => {
     toggleDisplay(e.target);
   });
 });
-
-//on click --> check if there is anything open
-
-//if it is, and it is the target, close the target
-
-//if it is, and it isnt the target, close the currOpen, and open the target
-
-//if it isnt, open the target
 
 function toggleDisplay(selected) {
   if (selected.classList.contains("active")) {
@@ -27,7 +18,6 @@ function toggleDisplay(selected) {
         faq.children[1].style.display = "none";
         faq.classList.remove("active");
         faq.children[0].children[1].src = "./assets/images/icon-plus.svg";
-        console.log("fired");
       }
     });
     selected.children[1].style.display = "block";
